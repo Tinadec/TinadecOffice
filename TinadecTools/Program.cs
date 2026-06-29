3cff46838c4ba3c662c0d42cd3ed23a9eaef711f
jimmy-sketch <jimmy163mail@163.com>
2026-06-29 08:08:08 +0800

    fix: 解决AOT问题，方法是暂时去掉AOT，后面需要反射的地方还不少

--- TinadecTools/Program.cs
+++ TinadecTools/Program.cs
@@ -18,7 +18,6 @@ while (true)
     {
         try
         {
-            // TODO: .NET AOT 兼容
             var req = JsonSerializer.Deserialize<ToolCallRequest<JsonElement>>(line)!;
             // TODO：搞定中间的东西
             var res = new JsonElement();
